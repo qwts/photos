50f4e540680b6716a6b08cc2fe3d18a691a5df2b
qwtm <91036491+qwtm@users.noreply.github.com>
2026-07-20 00:01:57 -0500

    Drop obsolete lightbox story wait

--- src/renderer/src/lightbox/Lightbox.stories.tsx
+++ src/renderer/src/lightbox/Lightbox.stories.tsx
@@ -221,7 +221,6 @@ export const NavigationPreservesViewIntentAndCloseResets: Story = {
     const landscape = canvas.getByRole('img', { name: 'IMG_4028.JPG' });
     await waitFor(() => expect(landscape).toHaveProperty('naturalWidth', 1280));
     await userEvent.click(canvas.getByRole('button', { name: 'Zoom in (+)' }));
-    await waitFor(() => expect(viewport).toHaveAttribute('data-zoom', '1.250'));
     await fireEvent.wheel(landscape, { deltaY: 5000 });
     await waitFor(() => expect(Number(viewport.dataset['panY'])).toBeLessThan(0));
     await userEvent.click(canvas.getByRole('button', { name: 'Rotate right (])' }));
